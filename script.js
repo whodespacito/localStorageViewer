7e952956d8468369fcfecebb06331397edfc9bd8
whodespacito <58792361+whodespacito@users.noreply.github.com>
2023-01-11 13:19:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let params = ""
+let params = "height=500,width=500,left=100,top=100,popup=yes"
 let window = open("about:blank", "viewer", params)
 let table = window.document.createElement
 window.document.write()
