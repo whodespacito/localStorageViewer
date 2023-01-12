e1795664a3a442e1a41d462856de6d55132cb02c
whodespacito <58792361+whodespacito@users.noreply.github.com>
2023-01-12 13:21:29 -0500

    finished

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-let params = "height=500,width=500,left=100,top=100,popup=yes"
-let window = open("about:blank", "viewer", params)
-let table = window.document.createElement
-window.document.write()
+let params = "height=500,width=500,left=100,top=100,popup=yes";
+let constructor = "<style> tr:nth-child(even) {background-color: #dddddd;} </style> <table> <tr> <th>index</th> <th>key</th> <th>value</th> </tr>";
+let index = 0;
+
+while (localStorage.key(index) !== null) {
+    constructor = constructor + ` <tr> <td> ${index} </td> <td> ${localStorage.key(index)} </td> <td> ${localStorage.getItem(localStorage.key(index))} </td> </tr>`;
+    index++;
+}
+
+let app = open("", "viewer", params);
+app.document.write(constructor);
